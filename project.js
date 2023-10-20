9fb050c1d8a4399b3b5fe7935130a1b843e2ef3a
Sebastianus Dimas A <93775124+SDA924@users.noreply.github.com>
2023-10-21 04:02:34 +0700

    Update project.js

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+
 var navHeight = $('nav').outerHeight();
 
 $(document).ready(function () {    
@@ -15,7 +16,7 @@ function scrollTo(href){
 }
 
 $('a.nav-link').click(function (e) { 
-    // e.preventDefault();
+   
     var href = $(this).attr("href");
     
     scrollTo(href);
@@ -23,7 +24,7 @@ $('a.nav-link').click(function (e) {
 
 
 var arrowVal=0;
-        function clickButton(arrow) {
+function clickButton(arrow) {
         // if(arrowVal<-360 && arrowVal<360)
         if (arrow =='left') {
 
@@ -35,7 +36,7 @@ var arrowVal=0;
             arrowVal+=120;
             $('img#circle').css( 'transform',`rotate(${arrowVal}deg)`);
 
-        }else if($(window).width() >= 1200){
+        }else if(arrow =='zoom' && $(window).width() >= 1200){
             $('button#project').removeClass('d-none');
             $('h2#Skill2').removeClass('d-none');
             $('h2#Skill1').addClass('d-none');
@@ -45,7 +46,7 @@ var arrowVal=0;
             $('button#zoom').addClass('d-none');
             $('img#circle').width('100%');
 
-        }else{
+        }else if(arrow =='zoom'){
             $('button#project').removeClass('d-none');
             $('h2#Skill1').addClass('d-none');
             $('div#carouselExample').removeClass('d-none');
@@ -54,23 +55,217 @@ var arrowVal=0;
             $('button#zoom').addClass('d-none');
             $('img#circle').width('200%');
 
+        }else if(arrow =='projectWeb'){
+            $('div#lingkaran').addClass('d-none');
+            $('div#projectWeb').removeClass('d-none');
+        }else if (arrow == 'projectMobile') {
+            $('div#lingkaran').addClass('d-none');
+            $('div#projectMobile').removeClass('d-none');
+        }else if (arrow == 'projectUIUX') {
+            $('div#lingkaran').addClass('d-none');
+            $('div#projectUIUX').removeClass('d-none');
+        }else if (arrow == 'close') {
+            $('div#lingkaran').removeClass('d-none');
+            $('div#projectWeb').addClass('d-none');
+            $('div#projectUIUX').addClass('d-none');
+            $('div#projectMobile').addClass('d-none');
         }
 
-        // $("img#circle").animate({
-        //     step: function(arrowVal,fx) {
-        //         $(this).css('transform',`rotate(${arrowVal}deg)`); 
-        //     }
-        // });
+    }
 
-        // $('img#circle').css( 'transform',`rotate(${arrowVal}deg)`);
+const toastTrigger = document.getElementById('toastImprovement')
+const toastLiveExample = document.getElementById('liveToast')
 
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
+// NAMA HOMEEEEE
+
+
+
+        function decodeText(){
+            var text = document.getElementsByClassName('decode-text')[0];
+            
+            var state = [];
+            for(var i = 0, j = text.children.length; i < j; i++ ){
+                text.children[i].classList.remove('state-1','state-2','state-3');
+                state[i] = i;
+            }
+        
+            
+            var shuffled = shuffle(state);
+         
+            for(var i = 0, j = shuffled.length; i < j; i++ ){
+                var child = text.children[shuffled[i]];
+                classes = child.classList;
+        
+               
+                var state1Time = Math.round( Math.random() * (2000 - 300) ) + 50;
+                if(classes.contains('text-animation')){ 
+                    setTimeout(firstStages.bind(null, child), state1Time);
+                }
+            }
         }
+        
+       
+        function firstStages(child){
+            if( child.classList.contains('state-2') ){   
+                child.classList.add('state-3');
+            } else if( child.classList.contains('state-1') ){
+                child.classList.add('state-2')
+            } else if( !child.classList.contains('state-1') ){
+                child.classList.add('state-1');
+                setTimeout(secondStages.bind(null, child), 100);
+            }    
+        }
+        function secondStages(child){
+            if( child.classList.contains('state-1') ){
+                child.classList.add('state-2')
+                setTimeout(thirdStages.bind(null, child), 100);
+            } 
+            else if( !child.classList.contains('state-1') )
+            {
+                child.classList.add('state-1')
+            }
+        }
+        function thirdStages(child){
+            if( child.classList.contains('state-2') ){
+                child.classList.add('state-3')
+            }
+        }
+        
+        function shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+        
+            
+            while (0 !== currentIndex) {
+                
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+        
+                
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+            return array;
+        }
+        
+        
+       
+        decodeText();
+        
+        
+        setInterval(function(){ decodeText(); }, 10000);
+
+
+// HEY_THEREE
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
 
 
 
-        const myCarousel = document.getElementById('carouselExample')
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    loop: true,
+    rotate: true,
+    mousewheel: {
+        invert: true,
+    },
+    
+  });
 
-        myCarousel.addEventListener('slide.bs.carousel', event => {
-          
 
-        })
+  var swiperProject = new Swiper('.slider', {
+    slidesPerView: 1,
+    speed: 1000,
+    pagination: {
+      el: '.slider__pagination',
+      clickable: true,
+    },
+    mousewheel: {
+        invert: true,
+    },
+    
+});
+
+
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
