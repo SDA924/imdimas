a70619c70db2183afa16dd084d848849e1511bea
Sebastianus Dimas A <93775124+SDA924@users.noreply.github.com>
2023-10-21 04:56:37 +0700

    Update project.js

--- project.js
+++ project.js
@@ -90,21 +90,24 @@ if (toastTrigger) {
 
         function decodeText(){
             var text = document.getElementsByClassName('decode-text')[0];
-            
+            // debug with
+            // console.log(text, text.children.length);
+        
+            // assign the placeholder array its places
             var state = [];
             for(var i = 0, j = text.children.length; i < j; i++ ){
                 text.children[i].classList.remove('state-1','state-2','state-3');
                 state[i] = i;
             }
         
-            
+            // shuffle the array to get new sequences each time
             var shuffled = shuffle(state);
          
             for(var i = 0, j = shuffled.length; i < j; i++ ){
                 var child = text.children[shuffled[i]];
                 classes = child.classList;
         
-               
+                // fire the first one at random times
                 var state1Time = Math.round( Math.random() * (2000 - 300) ) + 50;
                 if(classes.contains('text-animation')){ 
                     setTimeout(firstStages.bind(null, child), state1Time);
@@ -112,7 +115,7 @@ if (toastTrigger) {
             }
         }
         
-       
+        // send the node for later .state changes
         function firstStages(child){
             if( child.classList.contains('state-2') ){   
                 child.classList.add('state-3');
@@ -142,13 +145,13 @@ if (toastTrigger) {
         function shuffle(array) {
             var currentIndex = array.length, temporaryValue, randomIndex;
         
-            
+            // While there remain elements to shuffle...
             while (0 !== currentIndex) {
-                
+                // Pick a remaining element...
                 randomIndex = Math.floor(Math.random() * currentIndex);
                 currentIndex -= 1;
         
-                
+                // And swap it with the current element.
                 temporaryValue = array[currentIndex];
                 array[currentIndex] = array[randomIndex];
                 array[randomIndex] = temporaryValue;
@@ -157,10 +160,10 @@ if (toastTrigger) {
         }
         
         
-       
+        // Demo only stuff
         decodeText();
         
-        
+        // beware: refresh button can overlap this timer and cause state mixups
         setInterval(function(){ decodeText(); }, 10000);
 
 
@@ -227,8 +230,11 @@ var TxtRotate = function(el, toRotate, period) {
   var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     loop: true,
-    grabCursor: true,
     rotate: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     mousewheel: {
         invert: true,
     },
